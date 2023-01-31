fc0897babfccab6758533cc9682d0303558e503a
Shadryus <shadryus@gmail.com>
2023-01-30 21:20:50 -0300

    Adicionado layout dos dias

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,18 @@
 import './styles/global.css';
+import {Plus} from 'phosphor-react'
 
-import { Habit } from './components/Habit'
+// import { Habit } from './components/Habit'
+import logoImage from './assets/logo.svg'
+import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
 
-function App() {
+export function App() {
   return (
-    <div>
-      <Habit completed={3}/>
-      <Habit completed={10}/>
-      <Habit completed={7}/>
-      <Habit completed={78}/>
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   )
 }
-
-export default App
